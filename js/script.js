a8afe35466882c9a164a6a4a5d556650e82aadc3
Adris3 <advaitris@gmail.com>
2026-02-09 15:57:31 -0500

    Changed it so that visitors to the site are directed to the repo instead of the finished product

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 
 const projects = [
-    { title: "Pomodoro Activity Reminder Website", desc: "The P.A.R is a program that will remind you to do something physical every work cycle. You will give it four exercises that you could easily do 10-20 repititions of. You must do the randomly chosen exercise for the randomly chosen number of reps during the pomodoro break.", link:"https://adris3.github.io/ActivityReminder/" }
+    { title: "Pomodoro Activity Reminder Website", desc: "The P.A.R is a program that will remind you to do something physical every work cycle. You will give it four exercises that you could easily do 10-20 repititions of. You must do the randomly chosen exercise for the randomly chosen number of reps during the pomodoro break.", repolink:"https://github.com/Adris3/ActivityReminder?tab=readme-ov-file" }
 ];
 
 const container = document.getElementById("project_list")
 
 projects.forEach(p => {
     const div = document.createElement("div");
-    div.innerHTML = `<h3>${p.title}</h3><p>${p.desc}</p><a href=${p.link}>View ${p.title}</a>`;
+    div.innerHTML = `<h3>${p.title}</h3><p>${p.desc}</p><a href=${p.repolink}>View ${p.title} repo</a>`;
     container.appendChild(div);
 })
