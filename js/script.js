1547b8c7c0aec0f746f1d4a2fafa7941577fe01f
Adris3 <advaitris@gmail.com>
2026-02-09 15:50:59 -0500

    Added description for a project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 const projects = [
-    { title: "One", desc: "Project One" },
+    { title: "Pomodoro Activity Reminder Website", desc: "The P.A.R is a program that will remind you to do something physical every work cycle. It will pick a random exercise out of four that you give it, then produce a random number between 10 and 20 inclusive. You must do the randomly chosen exercise for the randomly chosen number of reps." },
     { title: "Two", desc: "Project Two" }
 ];
 
