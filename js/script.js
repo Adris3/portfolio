d0cfb9609f8477ef0895c5dcb09beec9d89e7e43
Adris3 <advaitris@gmail.com>
2026-02-09 15:55:51 -0500

    Added link to project from portfolio site

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 
 const projects = [
-    { title: "Pomodoro Activity Reminder Website", desc: "The P.A.R is a program that will remind you to do something physical every work cycle. It will pick a random exercise out of four that you give it, then produce a random number between 10 and 20 inclusive. You must do the randomly chosen exercise for the randomly chosen number of reps." },
-    { title: "Two", desc: "Project Two" }
+    { title: "Pomodoro Activity Reminder Website", desc: "The P.A.R is a program that will remind you to do something physical every work cycle. You will give it four exercises that you could easily do 10-20 repititions of. You must do the randomly chosen exercise for the randomly chosen number of reps during the pomodoro break.", link:"https://adris3.github.io/ActivityReminder/" }
 ];
 
 const container = document.getElementById("project_list")
 
 projects.forEach(p => {
     const div = document.createElement("div");
-    div.innerHTML = `<h3>${p.title}</h3><p>${p.desc}</p>`;
+    div.innerHTML = `<h3>${p.title}</h3><p>${p.desc}</p><a href=${p.link}>View ${p.title}</a>`;
     container.appendChild(div);
 })
