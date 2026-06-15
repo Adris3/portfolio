758bb4ef525013589b0f2377bf9842b377280944
Advait Risbud <advaitris@example.com>
2026-06-15 13:52:14 -0400

    Added new project to project section.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 
 const projects = [
+    {
+        title: "Bookmark manager",
+        desc: "A full stack bookmark management app built with React, Vite, Flask, and SQLite.",
+        repolink: "https://github.com/Adris3/bookmark_manager"
+    },
     { 
         title: "Pomodoro Activity Reminder Website", 
         desc: "The P.A.R is a program that will remind you to do something physical every work cycle. You will give it four exercises that you could easily do 10-20 repititions of. You must do the randomly chosen exercise for the randomly chosen number of reps during the pomodoro break.", 
